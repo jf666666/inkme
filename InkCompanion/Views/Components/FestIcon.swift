a8b4c8a2628716372cf3b9152e362fa97acda195
jf666666 <jf6666666@icloud.com>
2023-12-10 21:31:58 +0800

    Title: Add Custom Color Options to Festival Icons
    Description:
    - Introduced functionality to alter colors of festival icons.
    - Enhanced user customization and interface personalization.
    - Implemented color selection tool for individual icon segments.
    - Updated UI to support dynamic color changes.

---
+++ InkCompanion/Views/Components/FestIcon.swift
@@ -0,0 +1,18 @@
+//
+//  FestIcon.swift
+//  InkCompanion
+//
+//  Created by 姜锋 on 12/10/23.
+//
+
+import SwiftUI
+
+struct FestIcon: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FestIcon()
+}
