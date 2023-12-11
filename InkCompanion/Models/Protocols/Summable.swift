481ee960fe056c19424b2dd3056a69fd3bedb213
jf666666 <jf6666666@icloud.com>
2023-12-11 19:46:58 +0800

    Title: Enhance Salmon Run Stats Interface, Add addCoopsStatus Function
    Description:
    - Enriched the Salmon Run statistics interface for better user insights.
    - Added `addCoopsStatus` function for detailed coop status tracking.
    - Imported enum definitions from tkgstrator repository for consistency.
    - Improved data representation and handling with new enums.
    - Streamlined interface elements for a more intuitive user experience.

---
+++ InkCompanion/Models/Protocols/Summable.swift
@@ -0,0 +1,8 @@
+//
+//  Summable.swift
+//  InkCompanion
+//
+//  Created by 姜锋 on 12/11/23.
+//
+
+import Foundation
---
+++ InkCompanion/Views/Coop/addCoopStatus.swift
@@ -0,0 +1,8 @@
+//
+//  addCoopStatus.swift
+//  InkCompanion
+//
+//  Created by 姜锋 on 12/11/23.
+//
+
+import Foundation
